d1998a422f374cf93a7625324ba5ce4dcb7b800b
BuildTools <unconfigured@null.spigotmc.org>
2016-10-18 23:50:04 -0700

    changed the cards a bit

---
+++ Assets/Scripts/ScreenChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScreenChanger : MonoBehaviour {
+
+	// Use this for initialization
+	public void Play()
+    {
+	    SceneManager.LoadScene( "Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
