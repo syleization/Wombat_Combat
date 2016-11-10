04f158b4913f05d8a045c1727549816bddee19fa
NathanLacey <nathn.lacey@hotmail.com>
2016-11-09 19:46:25 -0800

    Added card movement and popup from hand.

---
+++ Assets/Scripts/CardMove.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMove : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private CardPopUp Card;
+    private DeckOfCards Deck;
+
+
+    void Start()
+    {
+        Card = GetComponent<CardPopUp>();
+        Deck = FindObjectOfType<DeckOfCards>();
+    }
+
+    void OnMouseDown()
+    {
+        // When card is clicked it is no longer in hand
+        Deck.isHoldingCard = true;
+        screenPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        Deck.ResetHandCardPositions(Card);
+    }
+
+    void OnMouseDrag()
+    {
+        // Move the card around with the cursor
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z - 5);
+
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        transform.position = currentPosition;
+    }
+
+    void OnMouseUp()
+    {
+        // Check if you are releasing the card back to the hand
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // This raycast goes past the card to see the hand
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~(1 << LayerMask.NameToLayer("Card"))))
+        {
+            Debug.Log(hit.collider.tag.ToString());
+            if (hit.collider.tag == "Hand")
+            {
+                // Card is now in hand
+                transform.position = new Vector3((((float)Deck.GetHandSize() - 1) * 2) - 5, -5, ((float)Deck.GetHandSize() - 1) * -1);
+                Deck.isHoldingCard = false;
+                Card.cardIsDown = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CardPopUp.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardPopUp : MonoBehaviour
+{
+    DeckOfCards Deck;
+
+    Ray ray;
+    RaycastHit hit;
+
+    private bool waitingForCard = false;
+    private float heightChange = 3.0f;
+    private float waitTime = 0.1f;
+
+    public bool cardIsDown = true;
+
+    void Start()
+    {
+        Deck = FindObjectOfType<DeckOfCards>();
+    }
+
+    void OnMouseEnter()
+    {
+        if (cardIsDown && MouseIsWithinBounds() && !Deck.isHoldingCard)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Card")
+                {
+                    cardIsDown = false;
+                    ChangeYPosition(heightChange);
+                }
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if(!cardIsDown && !waitingForCard && !Deck.isHoldingCard)
+        {
+            waitingForCard = true;
+            StartCoroutine(WaitToPutCardDown(waitTime));
+        }
+    }
+
+    bool MouseIsWithinBounds()
+    {
+        return 
+            Input.mousePosition.x > 0 && 
+            Input.mousePosition.x < Screen.width &&
+            Input.mousePosition.y > 0 && 
+            Input.mousePosition.y < Screen.height;
+    }
+
+    void ChangeYPosition(float amount)
+    {
+        this.transform.position = new Vector3(transform.position.x, transform.position.y + amount, transform.position.z - amount);
+    }
+
+    IEnumerator WaitToPutCardDown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ChangeYPosition(-heightChange);
+        cardIsDown = true;
+        waitingForCard = false;
+    }
+}
