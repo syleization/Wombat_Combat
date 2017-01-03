a9c41e6855c2853a10680705ab770d7d5a864026
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 02:46:21 -0800

    More stuff for the store. Trying to make it so the gems variable goes between the scenes but haven't gotten it fully working just yet.

--- Assets/Scripts/CurrencyManager.cs
+++ Assets/Scripts/CurrencyManager.cs
@@ -2,7 +2,32 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class CurrencyManager : MonoBehaviour {
+public class CurrencyManager : MonoBehaviour
+{
+    public static int CurrentGems;
+    public Text GemsText;
 
-    
+    public static int CurrentCoins;
+    public Text CoinText;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        CurrentGems = 0;
+        CurrentCoins = 0;
+    }
+
+    void Update()
+    {
+        string Gems = ((int)CurrentGems).ToString();
+
+        GemsText.text = Gems;
+
+        string Coins = ((int)CurrentCoins).ToString();
+
+        CoinText.text = Coins;
+
+
+    }
+
 }
--- Assets/Scripts/StorePack.cs
+++ Assets/Scripts/StorePack.cs
@@ -5,25 +5,25 @@ public class StorePack : MonoBehaviour
 {
     public void PackOnegems()
     {
-        GemsUI.CurrentGems += 200;
+        CurrencyManager.CurrentGems += 200;
         Debug.Log("Buying Pack one");
     }
 
     public void PackTwogems()
     {
-        GemsUI.CurrentGems += 500;
+        CurrencyManager.CurrentGems += 500;
         Debug.Log("Buying Pack Two");
     }
 
     public void PackThreegems()
     {
-        GemsUI.CurrentGems += 800;
+        CurrencyManager.CurrentGems += 800;
         Debug.Log("Buying Pack Three");
     }
 
     public void PackFourgems()
     {
-        GemsUI.CurrentGems += 1100;
+        CurrencyManager.CurrentGems += 1100;
         Debug.Log("Buying Pack Four");
     }
 }
