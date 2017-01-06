6a5fb52e9232ba24953baa7373d58a1c2a291f18
BuildTools <unconfigured@null.spigotmc.org>
2017-01-05 22:15:31 -0800

    We now got a settings pop-up menu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/Scripts/DropDownVertical.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DropDownVertical : MonoBehaviour, IPointerClickHandler
+{
+    public RectTransform container;
+    public bool toggle;
+    public bool isOpen;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+        }
+        else
+        {
+            isOpen = false;
+        }
+    }
+
+    void Start()
+    {
+        container = transform.FindChild("Container").GetComponent<RectTransform>();
+        isOpen = false;
+    }
+
+    void Update()
+    {
+        Vector3 scale = container.localScale;
+        scale.y = Mathf.Lerp(scale.y, isOpen ? 1 : 0, Time.deltaTime * 12);
+        container.localScale = scale;
+    }
+}
