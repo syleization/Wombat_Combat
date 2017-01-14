93b01551aa1e6d6981eef2d8019468636fa43c99
Millar-LayCyle Jordan Albert <362521@AIV.EDMC.STU>
2017-01-13 16:08:48 -0800

    The banners are in and animated.

---
+++ Assets/Scripts/BannerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BannerMove : MonoBehaviour {
+
+  
+    public void PlayBanner()
+    {
+
+    }
+
+    public void ReactionBanner()
+    {
+
+    }
+
+    public void CombineBanner()
+    {
+
+    }
+}
