673cebee4499c0a8213e642167b7451e57694657
J3WD4Z <j3wd4z@hotmail.com>
2017-05-30 14:36:59 -0700

    [Animations] - menu animator fixed

--- Assets/Scripts/MenuAnimations.cs
+++ Assets/Scripts/MenuAnimations.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class MenuAnimations : MonoBehaviour
 {
     public GameObject Stuff;
-    Animator otherAnimator;
+    public Animator otherAnimator;
 
     public void PlayButton()
     {
         otherAnimator.SetBool("IsPlay", true);
+    }
+
+    public void BackButton()
+    {
+        if (otherAnimator.GetBool("IsSettings"))
+        {
+            otherAnimator.SetBool("IsSettings", false);
+        }
+        else if (otherAnimator.GetBool("IsTwoPlayers"))
+        {
+            otherAnimator.SetBool("IsTwoPlayers", false);
+        }
+        else if (otherAnimator.GetBool("IsThreePlayers"))
+        {
+            otherAnimator.SetBool("IsThreePlayers", false);
+        }
+        else if (otherAnimator.GetBool("IsFourPlayers"))
+        {
+            otherAnimator.SetBool("IsFourPlayers", false);
+        }
+        else
+        {
+            otherAnimator.SetBool("IsPlay", false);
+        }
     }
 
     public void SettingsButton()
