69d35c0ad7f6f9cc5497d388c5339c44ec39f6c0
J3WD4Z <j3wd4z@hotmail.com>
2017-05-30 16:02:50 -0700

    [Sound] - using the music now

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    private bool created = false;
+
+    private void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            created = true;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SceneLoaded;
+    }
+
+    private void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuStartUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuStartUp : MonoBehaviour
+{
+    public string startMusic;
+
+    private void Start()
+    {
+        SoundManager.Instance.PlaySound(startMusic);
+    }
+}
